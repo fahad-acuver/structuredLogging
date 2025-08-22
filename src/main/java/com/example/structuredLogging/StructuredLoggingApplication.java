43a79cb7800914df8f804030819b2f37d005a92f
fhdMd <fahdmhd1707@gmail.com>
2025-08-22 15:55:25 +0530

    Initial commit

---
+++ src/main/java/com/example/structuredLogging/StructuredLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.example.structuredLogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructuredLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StructuredLoggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/structuredLogging/StructuredLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.structuredLogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StructuredLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
